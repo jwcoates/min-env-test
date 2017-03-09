4dc19bacc3ecc5ebde8d492241753f4cf132b2af
jcoates <jcoates@extremenetworks.com>
2017-03-09 15:19:31 -0500

    Initial files.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # min-env-test
 Minimal snap to test environment keyword.
+
+ - git clone https://github.com/jwcoates/min-env-test.github
+ - cd min-env-test
+ - snapcraft
---
+++ echo-vars.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+echo "MY_RO_AREA = $MY_RO_AREA" 
+echo "MY_RO_AREA_BAD = $MY_RO_AREA_BAD" 
+echo "MY_RW_AREA = $MY_RW_AREA" 
+echo "MY_RW_AREA_BAD = $MY_RW_AREA_BAD" 
